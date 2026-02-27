ca4e0008d6b670d023f8d86e8e770aa57b92dcda
GahMarin1 <jaque1805.r@gmail.com>
2026-02-27 13:22:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
-// BOOT DIGITANDO
-const text = "DoxxerVideos=> boot";
+/* BOOT */
+const text = "Inicializando sistema...\nAcesso autorizado.\nCarregando DoxxerVideos...\n";
 const bootText = document.getElementById("bootText");
+const bootScreen = document.getElementById("bootScreen");
+
 let i = 0;
-
-function typeEffect() {
-  if (i < text.length) {
-    bootText.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeEffect, 50);
-  } else {
-    setTimeout(() => {
-      document.getElementById("bootScreen").style.display = "none";
-    }, 800);
-  }
+function typeBoot(){
+ if(i < text.length){
+   bootText.innerHTML += text.charAt(i);
+   i++;
+   setTimeout(typeBoot, 30);
+ } else {
+   setTimeout(()=>bootScreen.style.display="none",800);
+ }
 }
+typeBoot();
 
-typeEffect();
-
-// MATRIX
+/* MATRIX */
 const canvas = document.getElementById("matrix");
 const ctx = canvas.getContext("2d");
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-const letters = "01";
-const fontSize = 14;
+const letters = "アカサタナハマヤラワ0123456789";
+const fontSize = 16;
 const columns = canvas.width / fontSize;
-
 const drops = [];
-for (let x = 0; x < columns; x++)
-  drops[x] = 1;
 
-function draw() {
-  ctx.fillStyle = "rgba(0,0,0,0.05)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+for(let x=0;x<columns;x++) drops[x]=1;
 
-  ctx.fillStyle = "#0f0";
-  ctx.font = fontSize + "px monospace";
+function draw(){
+ctx.fillStyle="rgba(0,0,0,0.05)";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.fillStyle="#00ff88";
+ctx.font=fontSize+"px monospace";
 
-  for (let i = 0; i < drops.length; i++) {
-    const text = letters.charAt(Math.floor(Math.random() * letters.length));
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+for(let i=0;i<drops.length;i++){
+const text=letters.charAt(Math.floor(Math.random()*letters.length));
+ctx.fillText(text,i*fontSize,drops[i]*fontSize);
 
-    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
-      drops[i] = 0;
+if(drops[i]*fontSize>canvas.height&&Math.random()>0.975)
+drops[i]=0;
 
-    drops[i]++;
-  }
+drops[i]++;
+}
 }
 
-setInterval(draw, 35);
+setInterval(draw,33);
