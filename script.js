92de14dfd74d10daaa900c6f3b12b843d88d34fa
GahMarin1 <jaque1805.r@gmail.com>
2026-02-27 12:14:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,4 +20,16 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("ageModal").style.display = "none";
     document.getElementById("bootScreen").style.display = "none";
   };
+
+  // KONAMI touch/mobile (B A)
+  const konamiCode = ["B","A"];
+  let input = [];
+  document.addEventListener("click", (e)=>{
+    if(e.target.id === "btnB") input.push("B");
+    if(e.target.id === "btnA") input.push("A");
+    if(input.join(",") === konamiCode.join(",")){
+      alert("Easter Egg ativado!");
+      document.getElementById("konamiButtons").classList.add("hidden");
+    }
+  });
 });
