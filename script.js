a19074715d1a6ac8c71e921c96e930b25286d938
GahMarin1 <jaque1805.r@gmail.com>
2026-02-26 21:40:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Efeito piscando no título estilo hacker
-const title = document.getElementById("videoTitle");
+const titles = document.querySelectorAll(".videoTitle");
 
 setInterval(() => {
-    title.style.opacity = (title.style.opacity == "0.5") ? "1" : "0.5";
+    titles.forEach(title => {
+        title.style.opacity = (title.style.opacity == "0.5") ? "1" : "0.5";
+    });
 }, 800);
 
-// Log estilo terminal
 console.log("%c[DoxxerVideos] Sistema Online", "color: #00ff00; font-size: 16px;");
