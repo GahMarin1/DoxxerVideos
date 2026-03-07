d7bdf298c7aa8d5674c8ae5f12043809da020b1b
GahMarin1 <jaque1805.r@gmail.com>
2026-03-06 23:09:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ document.querySelectorAll(".card").forEach(card=>{
 
 const id = card.dataset.id;
 
-if(!id) return;
-
 const likeBtn = card.querySelector(".like");
 const dislikeBtn = card.querySelector(".dislike");
 
 let likes = localStorage.getItem(id+"_like") || 0;
 let dislikes = localStorage.getItem(id+"_dislike") || 0;
 
-if(likeBtn) likeBtn.querySelector("span").innerText = likes;
-if(dislikeBtn) dislikeBtn.querySelector("span").innerText = dislikes;
+likeBtn.querySelector("span").innerText = likes;
+dislikeBtn.querySelector("span").innerText = dislikes;
 
-likeBtn?.addEventListener("click",()=>{
+likeBtn.addEventListener("click",()=>{
 
 likes++;
 localStorage.setItem(id+"_like",likes);
@@ -21,7 +19,7 @@ likeBtn.querySelector("span").innerText = likes;
 
 });
 
-dislikeBtn?.addEventListener("click",()=>{
+dislikeBtn.addEventListener("click",()=>{
 
 dislikes--;
 localStorage.setItem(id+"_dislike",dislikes);
