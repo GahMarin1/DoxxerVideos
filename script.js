0e37a139310f2311dde0f474b12bf638fea5181c
GahMarin1 <jaque1805.r@gmail.com>
2026-03-31 15:08:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".card").forEach(card=>{
+
+const id = card.dataset.id;
+
+const likeBtn = card.querySelector(".like");
+const dislikeBtn = card.querySelector(".dislike");
+
+if(!likeBtn || !dislikeBtn) return;
+
+let likes = localStorage.getItem(id+"_like") || 0;
+let dislikes = localStorage.getItem(id+"_dislike") || 0;
+
+likeBtn.querySelector("span").innerText = likes;
+dislikeBtn.querySelector("span").innerText = dislikes;
+
+likeBtn.addEventListener("click",()=>{
+likes++;
+localStorage.setItem(id+"_like",likes);
+likeBtn.querySelector("span").innerText = likes;
+});
+
+dislikeBtn.addEventListener("click",()=>{
+dislikes--;
+localStorage.setItem(id+"_dislike",dislikes);
+dislikeBtn.querySelector("span").innerText = dislikes;
+});
+
+});
